c2b23d8391a7063478982e3dc904fc1348350f17
Hasan Imam <miimam@amazon.com>
2019-03-03 14:03:02 -0800

    Update utility class for PDEX100 relay
    Forgot to include this in last commit - this change updates the
    utility that prepares SuperBotP bot commands. Commands differ
    between 100iv ones and rest.

--- src/main/java/pogo/assistance/bot/responder/relay/pokedex100/VerifierBotUtils.java
+++ src/main/java/pogo/assistance/bot/responder/relay/pokedex100/VerifierBotUtils.java
@@ -1,11 +1,20 @@
 package pogo.assistance.bot.responder.relay.pokedex100;
 
+import static pogo.assistance.data.model.pokemon.PokedexEntry.Form.ALOLAN;
+import static pogo.assistance.data.model.pokemon.PokedexEntry.Gender.FEMALE;
+import static pogo.assistance.data.model.pokemon.PokedexEntry.Gender.MALE;
+
 import com.google.common.base.Preconditions;
 import lombok.experimental.UtilityClass;
 import pogo.assistance.data.model.pokemon.PokedexEntry;
 import pogo.assistance.data.model.pokemon.PokedexEntry.Form;
 import pogo.assistance.data.model.pokemon.PokemonSpawn;
 
+/**
+ * Example commands:
+ *  - "?df weedle 44.5555,55.4444" -> candy
+ *  - "?df [id] 44.5555,55.4444" -> candy
+ */
 @UtilityClass
 public class VerifierBotUtils {
 
@@ -13,15 +22,47 @@ public class VerifierBotUtils {
      * @return
      *      100iv post command for SuperBotP. Example command: "geodude cp445 m 44.5555,55.4444"
      */
-    public static String toVerifierBotCommand(final PokemonSpawn pokemonSpawn) {
-        Preconditions.checkArgument(pokemonSpawn.getIv().isPresent());
+    public static String toPerfectIvSpawnCommand(final PokemonSpawn pokemonSpawn) {
+        Preconditions.checkArgument(pokemonSpawn.getIv().isPresent() && pokemonSpawn.getIv().get() == 100.0);
         Preconditions.checkArgument(pokemonSpawn.getCp().isPresent());
-        return String.format("%s cp%d %s %f,%f",
+        return String.format("%s cp%d %s %f,%f n",
                 getNameSegment(pokemonSpawn.getPokedexEntry()),
                 pokemonSpawn.getCp().get(),
                 getGenderSegment(pokemonSpawn.getPokedexEntry()),
                 pokemonSpawn.getLatitude().toDegrees(),
                 pokemonSpawn.getLongitude().toDegrees());
+    }
+
+    public static String toImperfectIvSpawnCommand(final PokemonSpawn pokemonSpawn) {
+        Preconditions.checkArgument(!pokemonSpawn.getIv().isPresent() || pokemonSpawn.getIv().get() != 100.0);
+        final StringBuilder stringBuilder = new StringBuilder("?df");
+
+        /*
+         * Prefer ID over name, since names can potentially be non-english in current implementation. But no easy way to
+         * handle different forms (alolans, castform forms etc.) so prefer names in those cases.
+         */
+        if (pokemonSpawn.getPokedexEntry().getId() > 0
+                && pokemonSpawn.getPokedexEntry().getForms().isEmpty()
+                && !pokemonSpawn.getPokedexEntry().getName().toLowerCase().contains("unown")) {
+            stringBuilder.append(" ").append(pokemonSpawn.getPokedexEntry().getId());
+        } else {
+            stringBuilder.append(" ").append(getNameSegment(pokemonSpawn.getPokedexEntry()));
+        }
+
+        pokemonSpawn.getIv().ifPresent(iv -> stringBuilder.append(" IV").append(iv.intValue()));
+        pokemonSpawn.getCp().ifPresent(cp -> stringBuilder.append(" CP").append(cp));
+        pokemonSpawn.getLevel().ifPresent(level -> stringBuilder.append(" L").append(level));
+        if (pokemonSpawn.getPokedexEntry().getGender() == MALE || pokemonSpawn.getPokedexEntry().getGender() == FEMALE) {
+            stringBuilder.append(" ").append(getGenderSegment(pokemonSpawn.getPokedexEntry()));
+        }
+        stringBuilder.append(" ").append(pokemonSpawn.getLatitude().toDegrees()).append(",")
+                .append(pokemonSpawn.getLongitude().toDegrees());
+
+        if (pokemonSpawn.getIv().orElse(-1.0) >= 90.0) {
+            stringBuilder.append(" n"); // needed to mention verifier name for 90+ iv posts
+        }
+
+        return stringBuilder.toString();
     }
 
     /**
@@ -53,7 +94,17 @@ public class VerifierBotUtils {
             }
         }
 
-        if (pokedexEntry.getForms().contains(Form.ALOLAN)) {
+        if (name.toLowerCase().contains("unown")) {
+            /*
+             * Expected conversions:
+             *   - "unown" -> "unown"
+             *   - "Unown A" -> "UnownA"
+             *   - "Unown - B" -> "UnownB"
+             */
+            return name.replaceAll("[\\s\\-]", "");
+        }
+
+        if (pokedexEntry.getForms().contains(ALOLAN)) {
             return "alolan " + name;
         }
 
@@ -62,8 +113,8 @@ public class VerifierBotUtils {
 
     private static String getGenderSegment(final PokedexEntry pokedexEntry) {
         switch (pokedexEntry.getGender()) {
-            case MALE: return "m";
-            case FEMALE: return "f";
+            case MALE: return "M";
+            case FEMALE: return "F";
             case NONE: case UNKNOWN: return "";
             default: throw new UnsupportedOperationException("Unsupported gender: " + pokedexEntry.getGender());
         }
