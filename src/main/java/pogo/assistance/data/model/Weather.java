f409bd90807d94cc3fdd009a5cda49701468695b
Hasan Imam <miimam@amazon.com>
2019-03-15 09:37:00 -0700

    Add weather enum
    Missed to add this file in last commit. This enum is already in use.

---
+++ src/main/java/pogo/assistance/data/model/Weather.java
@@ -0,0 +1,13 @@
+package pogo.assistance.data.model;
+
+public enum Weather {
+
+    PARTLY_CLOUDY,
+    CLOUDY,
+    FOG,
+    RAIN,
+    SNOW,
+    SUNNY, CLEAR, // these two are same - sunny is used during day, clear at night
+    WINDY
+
+}
